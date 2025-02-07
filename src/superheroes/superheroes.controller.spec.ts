76b1618a75ae7002e905b870a43095f5b00503a7
Elnur Musazade <iamelnurmusazade@gmail.com>
2025-02-07 16:46:32 +0100

    add controller test

--- src/superheroes/superheroes.controller.spec.ts
+++ src/superheroes/superheroes.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SuperheroesController } from './superheroes.controller';
+import { SuperheroesModule } from './superheroes.module';
 
 describe('SuperheroesController', () => {
   let controller: SuperheroesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [SuperheroesModule],
       controllers: [SuperheroesController],
     }).compile();
 
@@ -14,5 +16,15 @@ describe('SuperheroesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a superhero', () => {
+    const createSuperheroDto = {
+      name: 'Superman',
+      superpower: 'Strength',
+      humilityScore: 7,
+    };
+    const result = controller.create(createSuperheroDto);
+    expect(result.message).toBe('Superhero created');
   });
 });
