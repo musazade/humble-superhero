4c458fad633b5221120ff2adbb152d87540cb3be
Elnur Musazade <iamelnurmusazade@gmail.com>
2025-02-07 15:51:14 +0100

    add endpoint comment hints

--- src/superheroes/superheroes.controller.ts
+++ src/superheroes/superheroes.controller.ts
@@ -2,18 +2,18 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SuperheroesService } from './superheroes.service';
 import { CreateSuperheroDto } from './dto/create-superhero.dto';
 
-@Controller('superheroes')
+@Controller('superheroes') // {baseurl}/superheroes
 export class SuperheroesController {
   constructor(private readonly superheroeService: SuperheroesService) {}
 
-  @Post()
+  @Post() // POST /superheroes
   create(@Body() createSuperheroDto: CreateSuperheroDto) {
     console.log('createSuperheroDto', createSuperheroDto);
     this.superheroeService.create(createSuperheroDto);
     return { message: 'Superhero created' };
   }
 
-  @Get()
+  @Get() // GET /superheroes
   findAll() {
     return this.superheroeService.findAll();
   }
