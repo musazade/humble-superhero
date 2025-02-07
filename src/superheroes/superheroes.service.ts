da8cf111532544acf99f1c4fbf1a9d2264bc72d2
Elnur Musazade <iamelnurmusazade@gmail.com>
2025-02-07 15:28:35 +0100

    add superheroes service
    add create method that pushes new superhero into the array
    add findall method that returns all superheroes sorted by descending humility scores

--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateSuperheroDto } from './dto/create-superhero.dto';
 @Injectable()
-export class SuperheroesService {}
+export class SuperheroesService {
+  private superheroes = [];
+
+  create(createSuperheroDto: CreateSuperheroDto) {
+    this.superheroes.push(createSuperheroDto);
+  }
+
+  findAll() {
+    return this.superheroes.sort((a, b) => b.humilityScore - a.humilityScore); // sort by humility score before returning (desc)
+  }
+}
