bf9fa29e6568d146806140477a0dcd357d9cd426
Elnur Musazade <iamelnurmusazade@gmail.com>
2025-02-07 16:28:21 +0100

    add test for findall method

--- src/superheroes/superheroes.service.spec.ts
+++ src/superheroes/superheroes.service.spec.ts
@@ -15,4 +15,19 @@ describe('SuperheroesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return superheroes sorted by humilityScore', () => {
+    service.create({ name: 'Ironman', superpower: 'Flying', humilityScore: 5 });
+    service.create({ name: 'Flash', superpower: 'Speed', humilityScore: 9 });
+    service.create({
+      name: 'Superman',
+      superpower: 'Strength',
+      humilityScore: 7,
+    });
+
+    const superheroes = service.findAll();
+    expect(superheroes[0].name).toBe('Flash'); // Highest humility score first
+    expect(superheroes[1].name).toBe('Superman');
+    expect(superheroes[2].name).toBe('Ironman');
+  });
 });
